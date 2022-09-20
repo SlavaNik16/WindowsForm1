099b484a25c9347401689c0a78c39246f93756c4
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-20 14:35:30 +0300

    Add files via upload

---
+++ WindowsForm1/WindowsForm1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace WindowsForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AdresBox = new System.Windows.Forms.TextBox();
+            this.ProgCheck = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AgeBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.butOk = new System.Windows.Forms.Button();
+            this.butHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(41, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NameBox.Location = new System.Drawing.Point(217, 21);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(371, 20);
+            this.NameBox.TabIndex = 1;
+            this.NameBox.TextChanged += new System.EventHandler(this.NameBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(41, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Adres";
+            // 
+            // AdresBox
+            // 
+            this.AdresBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AdresBox.Location = new System.Drawing.Point(217, 86);
+            this.AdresBox.Multiline = true;
+            this.AdresBox.Name = "AdresBox";
+            this.AdresBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.AdresBox.Size = new System.Drawing.Size(371, 68);
+            this.AdresBox.TabIndex = 3;
+            this.AdresBox.TextChanged += new System.EventHandler(this.AdresBox_TextChanged);
+            // 
+            // ProgCheck
+            // 
+            this.ProgCheck.AutoSize = true;
+            this.ProgCheck.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProgCheck.Location = new System.Drawing.Point(45, 186);
+            this.ProgCheck.Name = "ProgCheck";
+            this.ProgCheck.Size = new System.Drawing.Size(133, 27);
+            this.ProgCheck.TabIndex = 4;
+            this.ProgCheck.Text = "Programmer";
+            this.ProgCheck.UseVisualStyleBackColor = true;
+            this.ProgCheck.CheckedChanged += new System.EventHandler(this.ProgCheck_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(217, 237);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 27);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Femail";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(518, 237);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 27);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Male";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(42, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Age";
+            // 
+            // AgeBox
+            // 
+            this.AgeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AgeBox.Location = new System.Drawing.Point(217, 288);
+            this.AgeBox.Name = "AgeBox";
+            this.AgeBox.Size = new System.Drawing.Size(371, 20);
+            this.AgeBox.TabIndex = 8;
+            this.AgeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AgeBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(42, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Output";
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputBox.Location = new System.Drawing.Point(217, 342);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputBox.Size = new System.Drawing.Size(371, 96);
+            this.OutputBox.TabIndex = 10;
+            // 
+            // butOk
+            // 
+            this.butOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butOk.Enabled = false;
+            this.butOk.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butOk.Location = new System.Drawing.Point(686, 18);
+            this.butOk.Name = "butOk";
+            this.butOk.Size = new System.Drawing.Size(88, 33);
+            this.butOk.TabIndex = 11;
+            this.butOk.Text = "OK";
+            this.butOk.UseVisualStyleBackColor = true;
+            this.butOk.Click += new System.EventHandler(this.butOk_Click);
+            // 
+            // butHelp
+            // 
+            this.butHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butHelp.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.butHelp.Location = new System.Drawing.Point(686, 73);
+            this.butHelp.Name = "butHelp";
+            this.butHelp.Size = new System.Drawing.Size(88, 33);
+            this.butHelp.TabIndex = 12;
+            this.butHelp.Text = "HELP";
+            this.butHelp.UseVisualStyleBackColor = true;
+            this.butHelp.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.butHelp);
+            this.Controls.Add(this.butOk);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AgeBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.ProgCheck);
+            this.Controls.Add(this.AdresBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Николаев В.А.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AdresBox;
+        private System.Windows.Forms.CheckBox ProgCheck;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox AgeBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Button butOk;
+        private System.Windows.Forms.Button butHelp;
+    }
+}
+
---
+++ WindowsForm1/WindowsForm1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            butOk.Enabled = false;
+            NameBox.Tag = false;
+            AdresBox.Tag = false;
+            ProgCheck.Tag = false;
+            radioButton1.Tag = false;
+            radioButton2.Tag = false;
+            AgeBox.Tag = false;
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = "Name: Введите имя! \r\n";
+            output += "Adres: Введите свой адрес! \r\n";
+            output += "Programmer: Поставив галочку, вы соглашаетесь на соглашение! \r\n";
+            output += "Femail/Mela: Выберите пол! \r\n";
+            output += "Age: Введите свой возвраст! \r\n";
+            OutputBox.Text = output; 
+        }
+
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+
+            this.NameBox.BackColor = this.NameBox.Text.Length != 0 ? System.Drawing.SystemColors.Window : Color.Red;
+            this.NameBox.Tag = this.NameBox.Text.Length != 0;
+            TestCheck();
+
+        }
+
+        private void butOk_Click(object sender, EventArgs e)
+        {
+            string output;
+            output = $"Name: {this.NameBox.Text} \r\n";
+            output += $"Adres:{this.AdresBox.Text} \r\n";
+            output += $"Programmer: {this.ProgCheck.Checked} \r\n";
+            output += $"Sex: {(string)(this.radioButton1.Checked ? "Femail" : "Male")}! \r\n";
+            output += $"Age: {this.AgeBox.Text} \r\n";
+            OutputBox.Text = output;
+        }
+
+        private void TestCheck()
+        {
+            this.butOk.Enabled = (((bool)(NameBox.Tag)) &&
+                            ((bool)(AdresBox.Tag)) &&
+                            ((bool)(ProgCheck.Tag)) &&
+                            (((bool)(radioButton1.Tag)) ||
+                            ((bool)(radioButton2.Tag))) &&
+                            ((bool)(AgeBox.Tag)));
+        }
+
+        private void AdresBox_TextChanged(object sender, EventArgs e)
+        {
+            
+            AdresBox.BackColor = AdresBox.Text.Length != 0 ? System.Drawing.SystemColors.Window : Color.Red;
+            AdresBox.Tag = AdresBox.Text.Length != 0;
+            TestCheck();
+        }
+
+        private void ProgCheck_CheckedChanged(object sender, EventArgs e)
+        {
+          
+            if (!ProgCheck.Checked)
+            {
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+            }
+            else
+            {
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+            }
+            ProgCheck.BackColor = ProgCheck.Checked ? System.Drawing.SystemColors.Window : Color.Red;
+            ProgCheck.Tag = ProgCheck.Checked;
+            TestCheck();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            radioButton1.BackColor = System.Drawing.SystemColors.Window;
+            radioButton1.Tag = radioButton1.Checked;
+            TestCheck();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radioButton2.BackColor = System.Drawing.SystemColors.Window;
+            radioButton2.Tag = radioButton2.Checked;
+            TestCheck();
+        }
+
+        private void AgeBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox txt;
+            txt = (TextBox)sender;
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                AgeBox.Tag = true;
+            }
+            AgeBox.BackColor = AgeBox.Text.Length != 0 ? System.Drawing.SystemColors.Window : Color.Red;
+            TestCheck();
+        }
+    }
+}
